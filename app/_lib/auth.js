04d4cffa161800a8c6ca24dae9f8e11d5a7d7575
GaziRiad <ouhibimustapha36@gmail.com>
2024-08-12 08:12:36 +0100

    Protecting routes with NextAuth middleware

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -10,4 +10,10 @@ import Google from "next-auth/providers/google";
 
 export const { auth, handlers } = NextAuth({
   providers: [Google],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
 });
