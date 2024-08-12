2d0b4c464f461539ef3257ff654bb1e4b12dcfa1
GaziRiad <ouhibimustapha36@gmail.com>
2024-08-12 10:35:30 +0100

    Creating a new guest on first signin

--- app/_components/SignInButton.jsx
+++ app/_components/SignInButton.jsx
@@ -1,14 +1,21 @@
-function SignInButton() {
+import { signinAction } from "../_lib/actions";
+
+async function SignInButton() {
   return (
-    <button className='flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium'>
-      <img
-        src='https://authjs.dev/img/providers/google.svg'
-        alt='Google logo'
-        height='24'
-        width='24'
-      />
-      <span>Continue with Google</span>
-    </button>
+    <form action={signinAction}>
+      <button
+        className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium"
+        type="submit"
+      >
+        <img
+          src="https://authjs.dev/img/providers/google.svg"
+          alt="Google logo"
+          height="24"
+          width="24"
+        />
+        <span>Continue with Google</span>
+      </button>
+    </form>
   );
 }
 
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,19 +1,37 @@
+// import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
-// const authConfig = {
-//   providers: Google({
-//     clientId: process.env.AUTH_GOOGLE_ID,
-//     clientSecret: process.env.AUTH_GOOGLE_SECRET,
-//   }),
-// };
-
-export const { auth, handlers } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
       return !!auth;
     },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch {
+        console.log("NO GUEST");
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
   },
 });
